d25b45290f5fce64099f1af1aa74300deca6f3b1
FrederikDiamonD <frederikhandberg02@gmail.com>
2022-08-26 23:06:23 +0200

    Home Section finished for mobile.

--- js/main.js
+++ js/main.js
@@ -25,3 +25,12 @@ const linkAction = () => {
 }
 
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+// Change Background Header
+const scrollHeader = () => {
+    const header = document.getElementById('header')
+
+    this.scrollY >= 50 ? header.classList.add('header-bg') : header.classList.remove('header-bg')
+}
+
+window.addEventListener('scroll', scrollHeader)
