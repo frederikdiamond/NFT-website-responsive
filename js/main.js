85da937dabe31b693c58e8006af8e1419801cccf
FrederikDiamonD <frederikhandberg02@gmail.com>
2022-08-26 22:10:20 +0200

    Nav menu finished for mobile.

---
+++ js/main.js
@@ -0,0 +1,27 @@
+// Show Menu
+const navMenu = document.getElementById('nav-menu')
+const navToggle = document.getElementById('nav-toggle')
+const navClose = document.getElementById('nav-close')
+
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+// Hide Menu
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+// Remove Menu For Mobile
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () => {
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
